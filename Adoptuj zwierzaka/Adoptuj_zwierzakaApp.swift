c74f7eb95422ba871f0b85d510dd589ec55479de
Paweł Staniul <pstaniul@gmail.com>
2024-04-21 13:40:45 +0200

    Initial Commit

---
+++ Adoptuj zwierzaka/Adoptuj_zwierzakaApp.swift
@@ -0,0 +1,20 @@
+//
+//  Adoptuj_zwierzakaApp.swift
+//  Adoptuj zwierzaka
+//
+//  Created by Paweł Staniul on 21/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Adoptuj_zwierzakaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
