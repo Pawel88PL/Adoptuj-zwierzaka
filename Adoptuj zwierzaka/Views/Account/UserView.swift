1b4185a883c5659ee9ee9df368f0034fda9900c4
Paweł Staniul <pstaniul@gmail.com>
2024-05-03 10:33:33 +0200

    Poprawiono wyświetlanie panelu użytkownika

--- Adoptuj zwierzaka/Views/Account/UserView.swift
+++ Adoptuj zwierzaka/Views/Account/UserView.swift
@@ -9,20 +9,37 @@ import SwiftUI
 
 struct UserView: View {
     @EnvironmentObject var appState: AppState
+
     var body: some View {
         Form {
-            Section(header: Text("Informacje o użytkowniku")) {
-                Text("Imię: \(appState.userFirstName)")
-                Text("Nazwisko: \(appState.userSecondName)")
-                Text("Email: \(appState.userEmail)")
-                Text("Tel: \(appState.userPhoneNumber)")
-                if appState.userRole == "user" {
-                    Text("Rola: Użytkownik")
-                } else {
-                    Text("Rola: \(appState.userRole)")
+            Section(header: Text("Informacje o użytkowniku").font(.headline)) {
+                HStack {
+                    Image(systemName: "person.fill")
+                        .foregroundColor(.gray)
+                    Text("Imię: \(appState.userFirstName)")
                 }
-                
+                HStack {
+                    Image(systemName: "person.fill")
+                        .foregroundColor(.gray)
+                    Text("Nazwisko: \(appState.userSecondName)")
+                }
+                HStack {
+                    Image(systemName: "envelope")
+                        .foregroundColor(.gray)
+                    Text("Email: \(appState.userEmail)")
+                }
+                HStack {
+                    Image(systemName: "phone.fill")
+                        .foregroundColor(.gray)
+                    Text("Tel: \(appState.userPhoneNumber)")
+                }
+                HStack {
+                    Image(systemName: "person.crop.circle.badge.checkmark")
+                        .foregroundColor(.gray)
+                    Text("Rola: \(appState.userRole == "user" ? "Użytkownik" : "Administrator")")
+                }
             }
+            .font(.subheadline)
             
             Section {
                 Button(action: {
@@ -31,13 +48,14 @@ struct UserView: View {
                     Text("Wyloguj")
                         .foregroundColor(.red)
                 }
+                Button("Edytuj") {
+                    // Akcja edycji profilu
+                    print("Edytuj profil")
+                }
+                .foregroundColor(.blue)
             }
         }
-        .navigationBarTitle("Panel użytkownika")
-        .navigationBarItems(trailing: Button("Edytuj") {
-            // Akcja edycji profilu
-            print("Edytuj profil")
-        })
+        .navigationBarTitle("Panel użytkownika", displayMode: .inline)
     }
 }
 
